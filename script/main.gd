289d60b89a4877dafdd4671ed7062cd26accb5fd
Fabio Alessandrelli <fabio.alessandrelli@gmail.com>
2017-02-20 17:05:40 +0100

    Add impersonate test

--- script/main.gd
+++ script/main.gd
@@ -3,18 +3,39 @@ extends Node
 const NET = preload("res://script/network.gd")
 const SERVER = preload("res://scene/server.tscn")
 const CLIENT = preload("res://scene/client.tscn")
+const TESTS = preload("res://script/tests.gd")
+
+onready var _Serv = get_node("GridContainer/Server")
+onready var _C1 = get_node("GridContainer/Client1")
+onready var _C2 = get_node("GridContainer/Client2")
+onready var _C3 = get_node("GridContainer/Client3")
 
 var _tex = null
 var _tree = SceneTree.new()
 
 func _ready():
-	node_setup(get_node("GridContainer/Server"), SERVER)
-	node_setup(get_node("GridContainer/Client1"), CLIENT)
-	node_setup(get_node("GridContainer/Client2"), CLIENT)
-	node_setup(get_node("GridContainer/Client3"), CLIENT)
-	pass
+	node_setup(_Serv, SERVER)
+	node_setup(_C1, CLIENT)
+	node_setup(_C2, CLIENT)
+	node_setup(_C3, CLIENT)
+	TESTS.setup(_get_net(_Serv), [_get_net(_C1),_get_net(_C2),_get_net(_C3)])
 
 func node_setup(node, scene):
 	var net = NET.new()
 	net.set_name("network")
+	var inst = scene.instance()
 	node.start(scene.instance(), [net])
+	var t = node.get_sub_tree()
+	if t.get_root().has_node("Server"):
+		t.get_root().get_node("Server")._on_listen_toggled(true)
+		t.get_root().get_node("Server/Panel/config/listen").set_disabled(true)
+	else:
+		t.get_root().get_node("Client")._on_listen_toggled(true)
+		t.get_root().get_node("Client/Panel/config/connect").set_disabled(true)
+
+func _on_Impersonate_pressed():
+	TESTS.test_impersonate()
+
+func _get_net(node):
+	var t = node.get_sub_tree()
+	return t.get_root().get_node("network")
---
+++ script/tests.gd
@@ -0,0 +1,45 @@
+extends Node
+
+static func setup(server, clients):
+	Globals.set("test_server", server)
+	Globals.set("test_clients", clients)
+
+static func test_impersonate():
+	var clients = Globals.get("test_clients")
+	assert(clients.size()>0)
+	var client = clients[clients.size()-1] # Last client is the bad guy!
+
+	var id = client.get_tree().get_network_unique_id()
+	assert(id != 1)
+	assert(client._has_peer)
+
+	# Will pose as other client to server
+	for c in client.get_children():
+		if c.id != id:
+			# Client change set network mode
+			c.set_network_mode(NETWORK_MODE_MASTER)
+			c.rpc_id(1, "client_message", var2bytes(str(id) + " posing as " + str(c.id)))
+			# Client restore network mode for future tests
+			c.set_network_mode(NETWORK_MODE_SLAVE)
+
+	# Will pose as server to other clients
+	client.set_network_mode(NETWORK_MODE_MASTER)
+	for c in client.get_children():
+		if c.id != id:
+			client.rpc_id(c.id, "server_message", var2bytes(str(id) + " posing as server"))
+	client.set_network_mode(NETWORK_MODE_SLAVE)
+
+	# Will pose as a different client to other clients
+	var ids = []
+	for c in client.get_children():
+		ids.append(c.id)
+	
+	for c in client.get_children():
+		if c.id != id:
+			# Client change set network mode
+			c.set_network_mode(NETWORK_MODE_MASTER)
+			for other in ids:
+				if other != c.id and other != id: # I'm sneaky, I'll send to all except the real owner
+					c.rpc_id(other, "client_message", var2bytes(str(id) + " posing as " + str(c.id)))
+			# Client restore network mode for future tests
+			c.set_network_mode(NETWORK_MODE_SLAVE)
